ef5df188bbd3144aac5b551dcecbe812d4fa8425
mycreation6666 <gtrs43324@gmail.com>
2024-10-17 16:42:58 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "REPO_NAME=\$(basename \$(git rev-parse --show-toplevel)); python3 /workspaces/\$REPO_NAME/soul.py",
+    "postStartCommand": "REPO_NAME=\$(basename \$(git rev-parse --show-toplevel)); python3 /workspaces/\$REPO_NAME/nand.py",
     "customizations": {
         "vscode": {
             "settings": {
